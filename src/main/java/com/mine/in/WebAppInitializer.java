7b1ce25529f14b1ed796f2fc2cde8628913f8e38
tirthankar <tirtha2u@yahoo.co.in>
2017-04-20 00:37:17 +0530

    demo

--- src/main/java/com/mine/in/WebAppInitializer.java
+++ src/main/java/com/mine/in/WebAppInitializer.java
@@ -1,16 +1,20 @@
 package com.mine.in;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan("com.mine.in")
-public class WebAppInitializer{
+@SpringBootApplication
+public class WebAppInitializer extends SpringBootServletInitializer {
 
-    public static void main(String[] args) throws Exception{
-        SpringApplication.run(WebAppInitializer.class, args);
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(WebAppInitializer.class, args);
+	}
+
+	@Override
+	protected final SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(WebAppInitializer.class);
+	}
+
 }
--- src/main/java/com/mine/in/controller/MainController.java
+++ src/main/java/com/mine/in/controller/MainController.java
@@ -1,14 +1,19 @@
 package com.mine.in.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value="/",method = RequestMethod.GET)
     public String homepage(){
+    	logger.info("inside homepage");
         return "index";
     }
 }

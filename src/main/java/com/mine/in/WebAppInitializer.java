56c17de473c59cda48c0878bdb6e6f222a0cf657
Tirthankar Bhadra <tirthankar.bhadra@tcs.com>
2017-04-19 19:29:12 +0530

    first commit

---
+++ src/main/java/com/mine/in/WebAppInitializer.java
@@ -0,0 +1,14 @@
+package com.mine.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+public class WebAppInitializer{
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(WebAppInitializer.class, args);
+    }
+}
